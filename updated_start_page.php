12df59d79cb752b7d1525b180e939164df5a8da9
dream78671 <44059254+dream78671@users.noreply.github.com>
2021-10-26 15:51:01 +0100

    website template

---
+++ updated_start_page.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+
+    <title>Ermol </title>
+
+
+ 
+ <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+    <script
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCDFGy8U7mpVknuBqPDdR3b7zYNr0xxLgg&callback=initMap&libraries=&v=weekly"
+      defer
+    ></script>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+    <!-- Font Awesome CDN -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+
+
+container { 
+  height: 200px;
+  position: relative;
+  border: 3px solid green; 
+}
+
+.center {
+  margin: 0;
+  position: absolute;
+  top: 85%;
+  left: 50%;
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+}
+
+.button {
+  border: none;
+  color: white;
+  padding: 15px 105px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 20px;
+  margin: 10px 2px;
+  cursor: pointer;
+}
+
+.button1 {background-color: #FF0000;} /* Green */
+.button2 {background-color: #008CBA;} /* Blue */
+
+
+* {box-sizing: border-box;}
+
+body { 
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.header {
+  overflow: hidden;
+  background-color: #FF0000;
+  padding: 20px 10px;
+}
+
+.header a {
+  float: left;
+  color: white;
+  text-align: center;
+  padding: 12px;
+  text-decoration: none;
+  font-size: 18px; 
+  line-height: 25px;
+  border-radius: 4px;
+}
+
+.header a.logo {
+  font-size: 25px;
+  font-weight: bold;
+}
+
+.header a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.header a.active {
+  color: white;
+}
+
+.header-right {
+  float: right;
+}
+
+@media screen and (max-width: 500px) {
+  .header a {
+    float: none;
+    display: block;
+    text-align: left;
+  }
+  
+  .header-right {
+    float: none;
+  }
+}
+
+#map {
+  height: 100%;
+}
+
+/* Optional: Makes the sample page fill the window. */
+html,
+body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+</style>
+</head>
+
+
+<div class="header">
+  <a href="#default" class="logo">Ermol</a>
+  <div class="header-right">
+    <a class="active" href="#home">Log In</a>
+	    <a href="#ermolsignUp">Sign Up</a>
+		<a 
+				
+				onClick="window.location = 'https://play.google.com/store/apps/developer?id=Umar+Jeff'"
+				
+				<p
+				 src="https://homepages.cae.wisc.edu/~ece533/images/airplane.png" width="150" height="165"
+
+				href="#" class="">Learner </a>
+            
+			<a 	href = '#'class = ''>Instructor</a>
+							<a 	href = '#'class = ''>Contact Us</a>
+				<a 	href = '#'class = ''>Help</a>
+				<a 	href = '#'class = ''>About Us</a>
+				<a 	href = '#'class = ''>jeff</a>
+
+  </div>
+</div>
+
+
+  
+  
+ <body>
+ 
+
+
+    <div id="map"
+	
+
+	
+	
+	>
+
+
+	
+	</div>
+	 <div class="center">
+<button class="button button1">Start</button>
+  </div>
+
+
+	 
+	
+
+
+  </body>
+
+
+ <script>
+   let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+	   
+
+
+</script>
+
+</html>
+
+  
