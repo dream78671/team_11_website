241e506a4ea62ed4edd62e477c11e32277e8f698
zzee86 <94978880+zzee86@users.noreply.github.com>
2021-12-07 09:27:47 +0000

    Update style.js

--- js/style.js
+++ js/style.js
@@ -2,7 +2,6 @@
 // get elements using ID tags
 const btn = document.getElementById("mobile-menu-button");
 const menu = document.getElementById("mobile-menu");
-const ts = document.getElementById("testings");
 
 // add event listeners to toggle the hidden attribute
 btn.addEventListener("click", () => {
